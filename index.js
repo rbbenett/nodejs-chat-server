f276108e1c8392926d863074d3cd2d40167ae605
Robert <rbbenett0614@gmail.com>
2021-03-23 18:14:57 -0400

    Added helper.js file

--- index.js
+++ index.js
@@ -2,12 +2,26 @@ const app = require('express')();
 const http = require('http').createServer(app);
 const socketio = require('socket.io');
 const io = socketio(http);
+const { addUser } = require('./helper');
 const PORT = process.env.PORT || 5000;
 
 io.on('connection', (socket) => {
   console.log(socket.id);
   socket.on('create-room', name => {
     console.log(`The room name recieved is ${name}`)
+  })
+  socket.on('join', ({name, room_id, user_id}) => {
+    const {error, user} = addUser({
+      socket_id: socket.id,
+      name,
+      room_id,
+      user_id
+    })
+    if (error) {
+      console.log('Join Error', error)
+    } else {
+      console.log('Join User', user)
+    }
   })
 });
 
